0a4dfa537cd65a2b7a37e74a6a2ff476f221a4d2
Cezar Sîrbu <sirbunicolaecezar@gmail.com>
2025-10-24 02:58:42 +0300

    Update server.js

--- deploy-src/server.js
+++ deploy-src/server.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
+app.use('/', (req, res, next) => {
+  try {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  } catch (err) {
+    next(err);
+  }
+});
+
 const port = process.env.PORT || 3000
 console.log("Listening on port: " + port)
 app.listen(port);
